7131e94018bf568216abe4fc824838757442e0db
TheViero <128616582+TheViero@users.noreply.github.com>
2023-04-11 16:24:43 +0200

    dekotrator

---
+++ Декоратор/Декоратор/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Декоратор.Декоратор;
+
+namespace Декоратор
+{
+    public partial class Form1 : Form
+    {
+        private readonly Tree _tree;
+        public Form1()
+        {
+            InitializeComponent();
+
+            _tree = new Tree("Oak", 10, 50);
+
+            DisplayTree();
+        }
+
+        private void DisplayTree()
+        {
+            var pictureBox = new PictureBox
+            {
+                Width = 300,
+                Height = 300,
+                Location = new Point(10, 10),
+                SizeMode = PictureBoxSizeMode.Zoom
+            };
+
+            Controls.Add(pictureBox);
+
+            var image = new Bitmap(pictureBox.Width, pictureBox.Height);
+            pictureBox.Image = image;
+
+            var treeImageDecorator = new TreeImageDecorator(_tree, Properties.Resources.tree);
+            var treeHeightDecorator = new TreeHeightDecorator(treeImageDecorator, new Font("Arial", 14, FontStyle.Regular));
+
+            treeHeightDecorator.Display(pictureBox);
+        }
+
+    }
+}
---
+++ Декоратор/Декоратор/Декоратор.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Декоратор
+{
+    internal class Декоратор
+    {
+        public class Tree
+        {
+            private readonly string _name;
+            private readonly int _height;
+            private readonly int _age;
+
+            public Tree(string name, int height, int age)
+            {
+                _name = name;
+                _height = height;
+                _age = age;
+            }
+
+            public virtual void Display()
+            {
+                MessageBox.Show($"Tree name: {_name}, height: {_height}, age: {_age}");
+            }
+        }
+
+        public abstract class TreeDisplayDecorator : Tree
+        {
+            protected Tree _tree;
+
+            public TreeDisplayDecorator(Tree tree) : base("", 0, 0)
+            {
+                _tree = tree;
+            }
+
+            public override void Display()
+            {
+                _tree.Display();
+            }
+
+            public abstract void Display(PictureBox pictureBox);
+        }
+
+        public class TreeImageDecorator : TreeDisplayDecorator
+        {
+            private readonly Image _image;
+
+            public TreeImageDecorator(Tree tree, Image image) : base(tree)
+            {
+                _image = image;
+            }
+
+            public override void Display(PictureBox pictureBox)
+            {
+                pictureBox.Image = _image;
+                base.Display();
+            }
+        }
+
+        public class TreeHeightDecorator : TreeDisplayDecorator
+        {
+            private readonly Font _font;
+
+            public TreeHeightDecorator(Tree tree, Font font) : base(tree)
+            {
+                _font = font;
+            }
+
+            public override void Display(PictureBox pictureBox)
+            {
+                base.Display();
+
+                using (var graphics = Graphics.FromImage(pictureBox.Image))
+                {
+                    graphics.DrawString($"Height: {_tree.GetType().GetProperty("Height").GetValue(_tree)}", _font, Brushes.Black, 0, 0);
+                }
+            }
+        }
+    }
+}
